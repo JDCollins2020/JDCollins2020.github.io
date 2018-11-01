d9e08f3cbb34e461946dea565c0319449c622baf
JDCollins2020 <39606010+JDCollins2020@users.noreply.github.com>
2018-11-01 18:34:48 -0500

    Update date.js

--- date.js
+++ date.js
@@ -2,5 +2,6 @@ function setDate(){
   document.getElementById('date').innerHTML= Date();
 }
 function display(element){
+  console.log(element.src);
     document.getElementById('message').style.backgroundImage = element.src;
 }
