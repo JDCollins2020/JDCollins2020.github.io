82aab4c1191f8a55a2e45ccfd055398dd851df6b
JDCollins2020 <39606010+JDCollins2020@users.noreply.github.com>
2018-11-01 18:43:57 -0500

    Update gallery.js

--- Photography/gallery.js
+++ Photography/gallery.js
@@ -1,3 +1,4 @@
 function setDisplay(element){
-  document.getElementById('message').style.backgroundImage = element.src;
+  //document.getElementById('message').style.backgroundImage = element.src;
+  document.getElementById('message').style.backgroundImage = 'url(' + element.src + ')';
 }
