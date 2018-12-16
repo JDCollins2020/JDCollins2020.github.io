456e4431fb3486a403651ef955297711767ddd13
JDCollins2020 <39606010+JDCollins2020@users.noreply.github.com>
2018-12-16 14:21:40 -0600

    Update LIT205.js

--- data/LIT205.js
+++ data/LIT205.js
@@ -3,13 +3,13 @@ var wordCloud = {
   arr: new Array();
 }
 function preload() {
-  result = loadStrings('dante.txt');
+  wordCloud.arr = loadStrings('dante.txt');
 }
 
 function setup() {
   createCanvas(640, 480);
   var ind = floor(random(result.length));
-  text(result[ind], 100, 100, 80, 80);
+  text(wordCloud.arr[ind], 100, 100, 80, 80);
 }
 
 function draw() {
