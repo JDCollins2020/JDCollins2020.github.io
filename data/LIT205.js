e4669126ea20c696d2b2e9008882eec091523582
JDCollins2020 <39606010+JDCollins2020@users.noreply.github.com>
2018-12-16 14:32:10 -0600

    Update LIT205.js

--- data/LIT205.js
+++ data/LIT205.js
@@ -2,10 +2,16 @@ var result;
 function preload() {
   result = loadStrings('dante.txt');
 }
-
+function word(x,y,fill,size,value){
+  this.x = x;
+  this.fill = fill;
+  this.size = size;
+  this.value = value;
+}
 function setup() {
   createCanvas(640, 480);
   var ind = floor(random(result.length));
+  textSize(50);
   text(result[ind], 100, 100, 80, 80);
 }
 
