9e1f18c418f6d948099a12ad20729685828f2c77
JDCollins2020 <39606010+JDCollins2020@users.noreply.github.com>
2018-12-16 14:06:39 -0600

    Update LIT205.js

--- data/LIT205.js
+++ data/LIT205.js
@@ -5,16 +5,9 @@ function preload() {
 
 function setup() {
   createCanvas(640, 480);
-    background(200);
   var ind = floor(random(result.length));
-  text(result[ind], 10, 10, 80, 80);
+  text(result[ind], 100, 100, 80, 80);
 }
 
 function draw() {
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(255);
-  }
-  ellipse(mouseX, mouseY, 80, 80);
 }
