aa53ec860e056035013a99c494fd406ee2019e10
JDCollins2020 <39606010+JDCollins2020@users.noreply.github.com>
2018-12-16 18:28:15 -0600

    Update LIT205.js

--- data/LIT205.js
+++ data/LIT205.js
@@ -1,18 +1,10 @@
-var result;
-function preload() {
-  result = loadStrings('dante.txt');
-}
-function word(x,y,fill,size,value){
-  this.x = x;
-  this.fill = fill;
-  this.size = size;
-  this.value = value;
-}
+var word = ["circle", "second", "against", "themselves", "bolgia", "myself", "within", "little", "things", "valley", "beheld", "already", "others", "onward", "itself", "before", "return", "turned", "seemed", "silent", "downward", "himself", "speech", "master", "another", "weeping", "whence", "through", "because", "people", "behind", "heaven", "father", "became", "replied", "spirit", "farther", "should", "desire", "leader", "behold", "without", "towards", "together", "saying", "wholly", "upward", "answer", "thyself", "bottom"];
+
 function setup() {
   createCanvas(640, 480);
-  var ind = floor(random(result.length));
+  var ind = floor(random(word.length));
   textSize(12);
-  text(result[ind], 100, 100, 80, 80);
+  text(word[ind], 100, 100, 80, 80);
 }
 
 function draw() {
